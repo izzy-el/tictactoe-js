b7f23d69dec5cb05ed0490d80ec29fbc1740caec
Izael <jv.izael@hotmail.com>
2021-09-22 16:57:24 -0300

    [feat] Implemented a checker in order to tell the user if he can or cannot put the X/O in the selected spot.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let determineElement = 0;
 
 function insert(id) {
-    if(determineElement % 2 == 0) {
-        document.getElementById(id).innerHTML = "x";
-    } else {
-        document.getElementById(id).innerHTML = "o";
+    if(document.getElementById(id).innerHTML !== "") {
+        window.alert("This field is not empty! Please select a valid field.");
     }
+    else {
+        if(determineElement % 2 == 0) {
+            document.getElementById(id).innerHTML = "X";
+        } else {
+            document.getElementById(id).innerHTML = "O";
+        }
+    }    
+
     determineElement++;
 }
