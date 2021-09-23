a949d8207b3d78137197107e9c5301ba9f215f87
Izael <jv.izael@hotmail.com>
2021-09-22 22:53:09 -0300

    [feat] Implemented a checker to see who won the game: Player 1 or Player 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let determineElement = 0;
-let boardGame = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+let determineElement = 0; //Used to determine if it is going to be a 'X' or a 'O' 
+let boardGame = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] //Initial values for the array representing the board game
+let isOver = false; //Inform if the game is over or not
 
 function insert(id) {
     if(document.getElementById(id).innerHTML != "") {
@@ -16,7 +17,11 @@ function insert(id) {
 
         determineElement++;
     }
+
+    checkWinner(boardGame);
     
+    console.log(boardGame[0][0], boardGame[0][1], boardGame[0][2]);
+
 }
 
 function addArray(id, symbol) {
@@ -29,4 +34,77 @@ function addArray(id, symbol) {
     }
 
     console.log(boardGame);
+}
+
+function checkWinner(array) {
+    //It checks every possible way a player can win the game
+    if(array[0][0] == 'X' && array[0][1] == 'X' && array[0][2] == 'X') {
+        window.alert("Player 1 Won!");
+        isOver = true;
+    }
+    else if(array[1][0] == 'X' && array[1][1] == 'X' && array[1][2] == 'X') {
+        window.alert("Player 1 Won!");
+        isOver = true;
+    }
+    else if(array[2][0] == 'X' && array[2][1] == 'X' && array[2][2] == 'X') {
+        window.alert("Player 1 Won!");
+        isOver = true;
+    }
+    else if(array[0][0] == 'X' && array[1][0] == 'X' && array[2][0] == 'X') {
+        window.alert("Player 1 Won!");
+        isOver = true;
+    }
+    else if(array[0][1] == 'X' && array[1][1] == 'X' && array[2][1] == 'X') {
+        window.alert("Player 1 Won!");
+        isOver = true;
+    }
+    else if(array[0][2] == 'X' && array[1][2] == 'X' && array[2][2] == 'X') {
+        window.alert("Player 1 Won!");
+        isOver = true;
+    }
+    else if(array[0][0] == 'X' && array[1][1] == 'X' && array[2][2] == 'X') {
+        window.alert("Player 1 Won!");
+        isOver = true;
+    }
+    else if(array[0][2] == 'X' && array[1][1] == 'X' && array[2][0] == 'X') {
+        window.alert("Player 1 Won!");
+        isOver = true;
+    }
+    else if(array[0][0] == 'O' && array[0][1] == 'O' && array[0][2] == 'O') {
+        window.alert("Player 2 Won!");
+        isOver = true;
+    }
+    else if(array[1][0] == 'O' && array[1][1] == 'O' && array[1][2] == 'O') {
+        window.alert("Player 2 Won!");
+        isOver = true;
+    }
+    else if(array[2][0] == 'O' && array[2][1] == 'O' && array[2][2] == 'O') {
+        window.alert("Player 2 Won!");
+        isOver = true;
+    }
+    else if(array[0][0] == 'O' && array[1][0] == 'O' && array[2][0] == 'O') {
+        window.alert("Player 2 Won!");
+        isOver = true;
+    }
+    else if(array[0][1] == 'O' && array[1][1] == 'O' && array[2][1] == 'O') {
+        window.alert("Player 2 Won!");
+        isOver = true;
+    }
+    else if(array[0][2] == 'O' && array[1][2] == 'O' && array[2][2] == 'O') {
+        window.alert("Player 2 Won!");
+        isOver = true;
+    }
+    else if(array[0][0] == 'O' && array[1][1] == 'O' && array[2][2] == 'O') {
+        window.alert("Player 2 Won!");
+        isOver = true;
+    }
+    else if(array[0][2] == 'O' && array[1][1] == 'O' && array[2][0] == 'O') {
+        window.alert("Player 2 Won!");
+        isOver = true;
+    }
+    else if(isOver == false && determineElement == 9) {
+        isOver = true;
+        window.alert("It is a TIE!");
+    }
+
 }
